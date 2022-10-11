7745843c5ba0a500e1d7e856f32817fff2125a42
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-10-11 14:03:14 +0200

    tests-unit pass

--- {{cookiecutter.project_slug}}/tests/unit/test_folder_structure.py
+++ {{cookiecutter.project_slug}}/tests/unit/test_folder_structure.py
@@ -9,6 +9,6 @@ from service_integration.pytest_plugin.folder_structure import (
     assert_path_in_repo, get_expected_files)
 
 
-@pytest.mark.parametrize("expected_path", get_expected_files("python"))
+@pytest.mark.parametrize("expected_path", get_expected_files("{{ cookiecutter.docker_base.split(":")[0] }}"))
 def test_path_in_repo(expected_path: str, project_slug_dir: Path):
     assert_path_in_repo(expected_path, project_slug_dir)
