cf2b5f3b63fe2e8af389cc0aa0ed7921a1c0fd8a
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-10-13 20:43:45 +0200

    cleanup test

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -1,29 +1,36 @@
-# pylint: disable=W0621
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
 
 import json
-import logging
+
 import subprocess
 import sys
 from pathlib import Path
 
+from pytest_cookies.plugin import Cookies, Result
 import pytest
 
 current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
-logger = logging.getLogger(__name__)
+repo_basedir =current_dir.parent
+cookiecutter_json = repo_basedir / "cookiecutter.json"
 
 
-def test_project_tree(cookies):
+
+def test_minimal_config_to_bake(cookies: Cookies):
     result = cookies.bake(extra_context={"project_slug": "test_project"})
     assert result.exit_code == 0
     assert result.exception is None
     assert result.project.basename == "test_project"
 
+    print(f"{result}", f"{result.context=}")
+
 
 @pytest.fixture(
-    params=json.loads((current_dir / "../cookiecutter.json").read_text())["docker_base"]
+    params=json.loads(cookiecutter_json.read_text())["docker_base"]
 )
-def baked_project(cookies, request):
-    return cookies.bake(
+def baked_project(cookies: Cookies, request) -> Result:
+    result = cookies.bake(
         extra_context={
             "project_slug": "DummyProject",
             "project_name": "dummy-project",
@@ -32,21 +39,20 @@ def baked_project(cookies, request):
         }
     )
 
+    assert result.exception is None
+    assert result.exit_code == 0
+    return result
+
 
 @pytest.mark.parametrize(
     "commands_on_baked_project",
     (
-        "ls -la .",
-        "make help",
-        "make devenv",
-        "make devenv; source .venv/bin/activate && make build up",
-        "make devenv; source .venv/bin/activate && build-devel up-devel",
-        "make info-build",
-        "make devenv; source .venv/bin/activate && build tests",
+        "ls -la .; make help",
+        "make devenv; source .venv/bin/activate && make build info-build test",
     ),
 )
-def test_make_workflows(baked_project, commands_on_baked_project: str):
-    working_dir = Path(baked_project.project)
+def test_make_workflows(baked_project: Result, commands_on_baked_project: str):
+    working_dir = baked_project.project_path
     subprocess.run(
-        ["/bin/bash", "-c", commands_on_baked_project], cwd=working_dir, check=True
+        ["/bin/bash", "-c", commands_on_baked_project], cwd=working_dir, check=True, capture_output=True
     )
