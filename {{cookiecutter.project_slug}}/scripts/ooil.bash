58ca283054d9f5b03120fa47522f14b0e46a675a
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-10-30 23:38:04 +0100

    updates ooil

--- {{cookiecutter.project_slug}}/scripts/ooil.bash
+++ {{cookiecutter.project_slug}}/scripts/ooil.bash
@@ -6,8 +6,7 @@ set -o nounset
 set -o pipefail
 IFS=$'\n\t'
 
-IMAGE_NAME="itisfoundation/service-integration:master-github-latest"
-IMAGE_NAME="local/service-integration:production"
+IMAGE_NAME="${DOCKER_REGISTRY:-itisfoundation}/service-integration:${DOCKER_IMAGE_TAG:-master-github-latest}"
 WORKDIR="$(pwd)"
 
 run() {
