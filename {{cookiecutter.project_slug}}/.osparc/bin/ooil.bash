0097e8a7e602ed14a942d74bf3d3f9ba19585246
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-11-23 18:31:12 +0100

    fix VERSION issue

--- {{cookiecutter.project_slug}}/.osparc/bin/ooil.bash
+++ {{cookiecutter.project_slug}}/.osparc/bin/ooil.bash
@@ -9,9 +9,22 @@ IFS=$'\n\t'
 IMAGE_NAME="itisfoundation/service-integration:${OOIL_IMAGE_TAG:-master-github-latest}"
 WORKDIR="$(pwd)"
 
-run() {
+run_with_color() {
   docker run \
     -it \
+    --rm \
+    --pull=newer \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --user="$(id --user "$USER")":"$(id --group "$USER")" \
+    --volume "$WORKDIR":/src \
+    --workdir=/src \
+    "$IMAGE_NAME" \
+    "$@"
+}
+
+run() {
+  docker run \
     --rm \
     --pull=newer \
     --volume="/etc/group:/etc/group:ro" \
