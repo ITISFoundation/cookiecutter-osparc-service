c3e10cc1b3cc74b123519ac4cd504f5fa2a7d2bd
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-11-23 23:17:11 +0100

    @sanderegg review: doct

--- {{cookiecutter.project_slug}}/.osparc/bin/ooil.bash
+++ {{cookiecutter.project_slug}}/.osparc/bin/ooil.bash
@@ -9,19 +9,13 @@ IFS=$'\n\t'
 IMAGE_NAME="itisfoundation/service-integration:${OOIL_IMAGE_TAG:-master-github-latest}"
 WORKDIR="$(pwd)"
 
-run_with_color() {
-  docker run \
-    -it \
-    --rm \
-    --pull=newer \
-    --volume="/etc/group:/etc/group:ro" \
-    --volume="/etc/passwd:/etc/passwd:ro" \
-    --user="$(id --user "$USER")":"$(id --group "$USER")" \
-    --volume "$WORKDIR":/src \
-    --workdir=/src \
-    "$IMAGE_NAME" \
-    "$@"
-}
+#
+# NOTE: with --interactive --tty the command below will
+#    produce colors in the outputs. The problem is that
+# .   ooil.bash >VERSION will insert special color codes
+# .   in the VERSION file which make it unusable as a variable
+# .   when cat VERSION !!
+#
 
 run() {
   docker run \
