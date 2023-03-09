2b15830296208aed0c2ad5dacb08dd57c31d9ca9
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-03-09 10:30:54 +0100

    fixes #99

--- {{cookiecutter.project_slug}}/.osparc/bin/ooil.bash
+++ {{cookiecutter.project_slug}}/.osparc/bin/ooil.bash
@@ -19,8 +19,9 @@ WORKDIR="$(pwd)"
 
 run() {
   docker run \
+    --tty \
     --rm \
-    --pull=newer \
+    --pull=always \
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --user="$(id --user "$USER")":"$(id --group "$USER")" \
