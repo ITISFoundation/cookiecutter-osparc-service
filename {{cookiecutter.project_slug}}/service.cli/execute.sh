7c2dd45cee47c823bf2ff523a29f30efc9ee42ec
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-03-10 11:27:53 +0100

    cleaning && adding necessary new files

--- {{cookiecutter.project_slug}}/service.cli/execute.sh
+++ {{cookiecutter.project_slug}}/service.cli/execute.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 IFS=$(printf '\n\t')
 
-cd /home/scu/{{ cookiecutter.project_package_name }}
+cd /home/scu/src/{{ cookiecutter.project_package_name }}
 
 echo "starting service as"
 echo   User    : "$(id "$(whoami)")"
@@ -12,25 +12,19 @@ echo   Workdir : "$(pwd)"
 echo "..."
 echo
 # ----------------------------------------------------------------
-# This script shall be modified according to the needs in order to run the service
-# The inputs defined in ${INPUT_FOLDER}/inputs.json are available as env variables by their key in capital letters
-# For example: input_1 -> $INPUT_1
+# This script shall be modified according to the needs in order to run the service. You can use the inputs in ${INPUT_FOLDER}
+# then retrieve the output and move it to the $OUTPUT_FOLDER as defined in the output labels
+# For example: cp output.csv $OUTPUT_FOLDER or to $OUTPUT_FOLDER/outputs.json using jq
+# TODO: Replace following
 
 # put the code to execute the service here
 # For example:
 env
 ls -al "${INPUT_FOLDER}"
+# or for example, to execute a python script on some input data:
+env
+python3 myscript.py -i path/to/input_data/input_file.txt
+cp output_file.json "${OUTPUT_FOLDER}"/outputs.json 
 
-# then retrieve the output and move it to the $OUTPUT_FOLDER
-# as defined in the output labels
-# For example: cp output.csv $OUTPUT_FOLDER or to $OUTPUT_FOLDER/outputs.json using jq
-#TODO: Replace following
-cat > "${OUTPUT_FOLDER}"/outputs.json << EOF
-{
-{%- for i in range(cookiecutter.number_of_outputs|int) -%}
-{%- if i > 0 -%},{%- endif %}
-    "output_{{ i+1 }}":"some_stuff"
-{%- endfor %}
-}
 EOF
 
